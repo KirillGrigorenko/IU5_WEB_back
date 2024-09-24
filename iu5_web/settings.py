7308f15c20d4738043980d7d1e478b48942874e9
Chichorenko <grigorenkokirill1533@mail.ru>
2024-09-24 15:52:36 +0000

    databases changes

--- iu5_web/settings.py
+++ iu5_web/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'iu5_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'group_schedule', # Имя вашей БД. Если вы создали черезе psql или IDE свою базу и хотите использовать его - пропишите его имя здесь
+        'USER': 'student',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': 5432, # Стандартный порт PostgreSQL
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
