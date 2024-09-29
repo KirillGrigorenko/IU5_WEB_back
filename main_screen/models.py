c2d05c40113441f256bd666356c50595cb34594c
Chichorenko <grigorenkokirill1533@mail.ru>
2024-09-29 18:51:27 +0000

    lab1 Правки

--- main_screen/models.py
+++ main_screen/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.template.defaultfilters import register
+from django.utils import timezone 
+
+@register.filter
+def multiply_minus_one(value):
+    """Умножает значение на два и вычитает единицу."""
+    return int(value) * 2 - 1
--- main_screen/views.py
+++ main_screen/views.py
@@ -2,34 +2,28 @@ from django.shortcuts import render, redirect
 from datetime import date
 
 groups = [
-{'id':1, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-51B", 'group_desc':"Сомнительно"},
-{'id':2, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-52B", 'group_desc':"Но окэй"},
-{'id':3, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-53B", 'group_desc':"Я понимаю"},
-{'id':4, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-54B", 'group_desc':"Что он делает"},
-{'id':5, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-55B", 'group_desc':"Но я не принимаю"},
-{'id':6, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-55B", 'group_desc':"НDEQW"}
+{'id':1, 'image':"http://127.0.0.1:9000/lab1/facultet/ИУ5.svg", 'group_fac':"ИУ", 'group_kaf':5, 'group_course':"3", 'group_count':"3", 'contact':"Власов А.А.", 'contact_phone':"79156734835"},
+{'id':4, 'image':"http://127.0.0.1:9000/lab1/facultet/ИУ5.svg", 'group_fac':"ИУ", 'group_kaf':5, 'group_course':"3", 'group_count':"5", 'contact':"Кузьмин Д.А.", 'contact_phone':"79190108127"},
+{'id':3, 'image':"http://127.0.0.1:9000/lab1/facultet/ИУ1.svg", 'group_fac':"ИУ", 'group_kaf':1, 'group_course':"3", 'group_count':"3", 'contact':"Беляев И.А.", 'contact_phone':"79858888218"},
+{'id':5, 'image':"http://127.0.0.1:9000/lab1/facultet/ИУ4.png", 'group_fac':"ИУ", 'group_kaf':4, 'group_course':"2", 'group_count':"3", 'contact':"Нагапетян В.С.", 'contact_phone':"79854605879"},
+{'id':2, 'image':"http://127.0.0.1:9000/lab1/facultet/ИУ4.png", 'group_fac':"ИУ", 'group_kaf':4, 'group_course':"4", 'group_count':"3", 'contact':"Григоренко К.Д.", 'contact_phone':"79166431639"},
+{'id':6, 'image':"http://127.0.0.1:9000/lab1/facultet/ИУ8.png", 'group_fac':"ИУ", 'group_kaf':8, 'group_course':"4", 'group_count':"2", 'contact':"Канев А.И.", 'contact_phone':""}
 ]
 
 orders = [
-    {'order_id': 1, 'order_date': '17.09.2024', 'groups':[
-        {'id':1, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-51B", 'group_desc':"Сомнительно"},
-        {'id':2, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-52B", 'group_desc':"Но окэй"},]},
-    {'order_id': 2, 'order_date': '16.09.2024', 'groups':[
-        {'id':5, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-55B", 'group_desc':"Но я не принимаю"},
-        {'id':6, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-55B", 'group_desc':"НDEQW"},]},
-    {'order_id': 3, 'order_date': '16.09.2024', 'groups':[
-        {'id':3, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-53B", 'group_desc':"Я понимаю"},
-        {'id':4, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-54B", 'group_desc':"Что он делает"},]},
-    {'order_id': 4, 'order_date': '16.09.2024', 'groups':[
-        {'id':1, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-51B", 'group_desc':"Сомнительно"},
-        {'id':4, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'group_name':"IU5-54B", 'group_desc':"Что он делает"},]}
-]
-
-index_photos = [
-    {'id':1, 'image':"http://127.0.0.1:9000/lab1/bmstu.png", 'alt': "bmstu.png"},
-    {'id':2, 'image':"http://127.0.0.1:9000/lab1/search.png", 'alt': "search.png"},
-    {'id':3, 'image':"http://127.0.0.1:9000/lab1/dashboard.png", 'alt': "dashboard.png"},
-    {'id':4, 'image':"http://127.0.0.1:9000/lab1/photo1.jpeg", 'alt': "photo1.jpeg"}
+    {'order_id': 1, 'order_date': "2024-09-21 17:00:00", 'classroom':"515ю", 'info': "asmentus wine tastes the same as i remember", 'groups':[
+        {'id':1},
+        {'id':2}]},
+    {'order_id': 2, 'order_date': "2024-09-21 17:00:00", 'classroom':"515ю", 'info': "asmentus wine tastes the same as i remember", 'groups':[
+        {'id':3},
+        {'id':4}]},
+    {'order_id': 3, 'order_date': "2024-10-01 08:00:00", 'classroom':"502ю", 'info': "asmentus wine tastes the same as i remember", 'groups':[
+        {'id':2},
+        {'id':5}]},
+    {'order_id': 4, 'order_date': "2024-10-03 10:00:00", 'classroom':"404", 'info': "asmentus wine tastes the same as i remember", 'groups':[
+        {'id':3},
+        {'id':4},
+        {'id':1}]},
 ]
 
 def groups_func(request):
@@ -46,28 +40,19 @@ def groups_func(request):
     { 'data': {
         'page_name': 'Группы',
         'groups' : filtered_groups,
-        'index_photo' : index_photos
     }})
 
 
 def info(request, id):
     group = next((gr for gr in groups if gr['id'] == id), None)
-    return render(request, 'info_group.html', {'data': {'group': group, 'index_photo':index_photos}})
+    return render(request, 'info_group.html', {'data': {'group': group}})
 
 
 def get_schedule(request, order_id):
-    order = next((order for order in orders if order['order_id'] == order_id), None)
+    order = next((order for order in orders if order['order_id'] == order_id), None) 
     if order is None:
         return render(request, '404.html', status=404)
-    return render(request, 'schedule.html', {'data': {'orders': orders, 'index_photo': index_photos, 'this': order}})
+    return render(request, 'schedule.html', {'data': {'groups': groups, 'this': order}})
 
-def schedule(request):
-    if request.method == 'GET':
-        order_id = request.GET.get('order_id')
-        if order_id is not None:
-            order_id = int(order_id)
-            print(order_id)
-            return get_schedule(request, order_id)
-    return render(request, 'schedule.html', {'data': {'orders': orders, 'index_photo': index_photos}})
 
 # Create your views here.
