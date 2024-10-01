adb926e1e78c452986c8cc287c6615ca71e2d80f
Chichorenko <grigorenkokirill1533@mail.ru>
2024-10-01 07:01:20 +0000

    попытка Джанго Админки

--- main_screen/admin.py
+++ main_screen/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
+from .models import Group, Order
 
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('group_fac', 'group_kaf', 'group_course', 'group_count', 'contact', 'contact_phone')
+    search_fields = ('group_fac', 'group_kaf', 'group_course', 'contact')
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'group', 'date', 'audience', 'teacher')
+    list_filter = ('date', 'group')
+    search_fields = ('audience', 'teacher')
+
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.clean()  # Проверка уникальности перед сохранением
+            super().save_model(request, obj, form, change)
+        except ValidationError as e:
+            form.add_error(None, e)
--- main_screen/models.py
+++ main_screen/models.py
@@ -1,9 +1,32 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-from django.template.defaultfilters import register
-from django.utils import timezone 
+class Group(models.Model):
+    group_fac = models.CharField(max_length=100)
+    group_kaf = models.CharField(max_length=100)
+    group_course = models.PositiveIntegerField()
+    group_count = models.PositiveIntegerField()
+    contact = models.CharField(max_length=100)
+    contact_phone = models.CharField(max_length=15)
 
-@register.filter
-def multiply_minus_one(value):
-    """Умножает значение на два и вычитает единицу."""
-    return int(value) * 2 - 1
+    def __str__(self):
+        return self.group_fac
+        
+    def save(self, *args, **kwargs):
+        if self.group_count > 50:
+            raise ValidationError("Group count cannot exceed 50.")
+        super().save(*args, **kwargs)
+
+class Order(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date = models.DateField()
+    audience = models.CharField(max_length=100)
+    teacher = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Order for {self.group} on {self.date}"
+
+    def save(self, *args, **kwargs):
+        if Order.objects.filter(group=self.group, date=self.date).exists():
+            raise ValidationError("An order for this group on this date already exists.")
+        super().save(*args, **kwargs)
